b77196aa955f2bd712034f848556d15abd6031ba
LeeH109 <hanhnguyen.10092002@gmail.com>
2023-06-19 15:02:50 +0700

    tailwind

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -52,27 +52,30 @@ const Cart = () => {
 
   return (
     <div className="w-full h-auto min-h-[680px] bg-gray-100 pb-8">
-      <div className="flex container justify-between mt-4 w-2/4  ml-[12vw]  mb-3">
+      <div className="flex container justify-between ml-[14vw] w-2/4 mt-4  mb-3 max-[600px]:ml-[0vw] max-[600px]:w-full 
+      max-[800px]:w-2/3   md:ml-[0vw] md:w-2/3 sm:ml-[0vw] sm:w-full  xl:w-2/4 xl:ml-[14vw] ">
         <div className="pt-20 text-3xl font-bold  "> Cart </div>
         <div
           onClick={cleanCart}
           className=" cursor-pointer pt-[87px] w-50  flex text-center align-center
-           text-black mr-2 item-center  duration-150 hover:text-red-500 "
+           text-black mr-2 item-center  duration-150 hover:text-red-500 xl:mr-[4vw]
+         "
         >
-          <BsFillTrash2Fill className="text-2xl  " />
-          <span className="font-medium ml-1 mr-1  ">Remove</span>
+          <BsFillTrash2Fill className="text-2xl mr-1   " />
+          <span className="font-medium  mr-[3vw] max-[600px]:mr-[12vw] md:mr-[-3vw] 
+          sm:mr-[-24vw] ">Remove</span>
         </div>
       </div>
 
       <div class="mx-auto max-w-[1100px] justify-center px-6 md:flex md:space-x-6 xl:px-0 container ">
-        <div class="rounded-lg md:w-2/3">
+        <div class="rounded-lg md:w-2/3 ">
           {cart.map((item) => {
             //   return <CartItem item={item} key={item.id } />;
             return <CartItem item={item} key={item.id} />;
           })}
         </div>
         <div class=" h-full rounded-lg border bg-white max-w-md p-6 shadow-md md:mt-6 md:w-1/3
-         sm:mt-10 sm:m-auto 
+         sm:mt-10 sm:m-auto  max-[600px]:mt-5 
         ">
           <div class="mb-2 flex justify-between">
             <p class="text-gray-700">Subtotal</p>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,7 @@ const Hero = () => {
       //     className="bg-green-100 h-[500px]
       //     bg-[url('https://aldoshoes.cdn.vccloud.vn/web/image/356434/Sale%20Banner_1000%20x%20400.jpg?access_token=3aff2e77-1d2a-463d-a70c-9adc7910b10d')]
       // bg-no-repeat bg-cover bg-center py-24   "
-      className=" h-[500px] mt-16 "
+      className=" h-[500px] mt-16 md:h-[400px] sm:h-[200px] max-[600px]:h-[200px]"
     >
       <div
         className="container mx-auto flex justify-around h-full 
